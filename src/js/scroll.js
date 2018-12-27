80ae50a3b2e68ded789165d987b8e61bc3ed80f4
abishek-sundar <abisheksundar2@gmail.com>
2018-12-27 14:54:02 +0400

    Added scrolling effect to nav and fixed to top

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,6 @@
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#main-navbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
+});
