20ac47a05725371ac94f1a13584322f2ff661b4a
abishek-sundar <abisheksundar2@gmail.com>
2018-12-29 01:47:05 +0400

    Small bug fix, might remove due to performance later

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,5 +2,22 @@ $(function () {
   $(document).scroll(function () {
     var $nav = $("#main-navbar");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    checkAndRemove();
   });
 });
+
+function checkAndRemove(){
+  var nav = document.querySelector("#main-navbar");
+  var scrollFound = false;
+  var blueFound = false;
+  for (var i = 0; i < nav.classList.length; i++){
+    if (nav.classList[i] === "scrolled"){
+      scrollFound = true;
+    }else if(nav.classList[i]==="blue-bg"){
+      blueFound = true;
+    }
+  }
+  if (scrollFound && blueFound){
+    nav.classList.remove("blue-bg");
+  }
+}
