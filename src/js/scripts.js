ea90a4aa8149308546d8dd6efd4f1bc768020b14
abishek-sundar <abisheksundar2@gmail.com>
2018-12-31 15:20:30 +0400

    Fixed resize nav bug

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,27 +2,25 @@ $(function () {
   $(document).scroll(function () {
     var $nav = $("#main-navbar");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    checkAndRemove();
   });
 });
-
-function checkAndRemove() {
-  var nav = document.querySelector("#main-navbar");
-  var scrollFound = false;
-  var blueFound = false;
-  for (var i = 0; i < nav.classList.length; i++) {
-    if (nav.classList[i] === "scrolled") {
-      scrollFound = true;
-    } else if (nav.classList[i] === "blue-bg") {
-      blueFound = true;
-    }
-  }
-  if (scrollFound && blueFound) {
-    nav.classList.remove("blue-bg");
-  }
-}
 
 function changeBg() {
   var nav = document.querySelector("#main-navbar");
   nav.classList.toggle("blue-bg");
 }
+
+function removeBlue(){
+  var nav = document.querySelector("#main-navbar");
+  var classes = nav.classList;
+  for (var i = 0; i < classes.length; i++){
+    if (classes[i] == "blue-bg"){
+      document.querySelector("button").click();
+    }
+  }
+}
+
+$(function(){
+  var mdEventListener = window.matchMedia("(max-width: 768px)");
+  mdEventListener.addListener(removeBlue);
+});
